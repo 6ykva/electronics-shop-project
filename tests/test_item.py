db80ee852d3ee39d2eac7d63559e1baf923957cf
alexandr <6ykva.izh@gmail.com>
2023-09-18 12:00:47 +0400

    add code in item, add test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,22 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+def test_Item():
+    item = Item('Television', 20000, 3)
+    assert item.name == 'Television'
+    assert item.price == 20000
+    assert item.quantity == 3
+
+
+def test_calculate_total_price():
+    item = Item('Television', 20000, 3)
+    assert item.calculate_total_price() == 60000
+
+
+def test_apply_discount():
+    item = Item('Television', 20000, 3)
+    Item.pay_rate = 1.5
+    item.apply_discount()
+    assert item.price == 30000
